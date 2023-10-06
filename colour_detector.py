b5a3585e83d810efb6c3265ec317e4d4134be7a1
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2023-10-07 00:12:24 +0100

    tried threading instead of asyncio coroutines, threading gives the same result.

--- colour_detector.py
+++ colour_detector.py
@@ -1,5 +1,6 @@
 # create class to detect a certain colour from image and return coordinates
-import asyncio
+import threading
+import time
 
 
 class ColourDetector:
@@ -8,6 +9,7 @@ class ColourDetector:
         self.received_image = None
         # for checking whether to keep running or to stop
         self._keep_running = False
+        self._thread = None
 
     # the property is public whereas the variable is private otherwise the setter will cause an infinite loop
     @property
@@ -21,10 +23,11 @@ class ColourDetector:
         print(self._keep_running)
         if value:
             print("run loop started")
-            asyncio.run(self._run())
+            self._thread = threading.Thread(self._run())
+            self._thread.start()
         else:
             print("run loop stopped")
-            self._run().close()
+            self._thread.join()
 
     # ProcessImage shouldn't be async as otherwise it might return variables with no value
     # should return maps of the image outlining each specific colour
@@ -35,10 +38,10 @@ class ColourDetector:
 
     # should not be called from outside the class as its private, the program should only change the keep_running
     # value and whenever that is changed _run will run if true and cancel if false
-    async def _run(self):
+    def _run(self):
         while self._keep_running:
             print("is looping")
             if self.received_image:
                 # get colour maps and return the values
                 colour_maps = self.process_image()
-            await asyncio.sleep(1)
+            time.sleep(1)
--- input_handler.py
+++ input_handler.py
@@ -1,25 +1,22 @@
-import asyncio
-
 from pynput.keyboard import Key, Listener
 
 
-class InputHandler():
+class InputHandler:
     def __init__(self):
-        # define a listener and start it
-        self._key_listener = Listener(on_press=self._process_inputs)
+        # define a listener and start it - should always be on as it's used for checking inputs for other cases
+        # the listener is a thread itself as well
+        self._key_listener = Listener(on_press=self._process_inputs, name="INPUT")
         self._key_listener.start()
-
+        # public dictionary of commands that each key press will have
         self.commands = dict()
 
     def _process_inputs(self, key):
-        print(key, "pressed")
         try:
             for command in self.commands[key]:
                 command()
-        except:
-            # if no commands present then add in an empty key-value pair for future commands
-            self.commands[key] = []
+        except: pass
 
+        print(key, "pressed")
         print(self.commands)
 
     def attach_func_to_keypress(self, key, func):
