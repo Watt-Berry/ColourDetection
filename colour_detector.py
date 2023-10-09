45dec157ac95b7ebc39ecea0332652547d701ab8
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2023-10-09 18:36:00 +0100

    changed test image to an image with flatter colours so it tests better as the different channels are more well shown. added channel getters which display the channel with opencv and destroy on a button press, but they're not asynchronous or threaded so they pause the program until they're closed.

--- colour_detector.py
+++ colour_detector.py
@@ -6,8 +6,12 @@ import cv2
 
 class ColourDetector:
     def __init__(self):
-        # public so any outside code should be able to add an image to it
+        # private but the image to process property is a public variable that returns this value,
+        # used to prevent any outside code from directly altering the value
         self._received_image = None
+
+        # private
+        self._colour_channels = {"BASE": None, "RED": None, "GREEN": None, "BLUE": None}
 
     # make the received_image variable a property
     @property
@@ -18,15 +22,76 @@ class ColourDetector:
     # values are returned
     @image_to_process.setter
     def image_to_process(self, value: str):
-        self._received_image = cv2.imread(value, flags=cv2.IMREAD_GRAYSCALE)
+        self._received_image = cv2.imread(value, flags=cv2.IMREAD_REDUCED_COLOR_2)
         self._process_image()
+
+    @property
+    def colour_channels(self):
+        return self._colour_channels
+
+    @colour_channels.getter
+    def base_channel(self):
+        try:
+            cv2.imshow("base", self._colour_channels["BASE"])
+            cv2.waitKey(0)
+            cv2.destroyWindow("base")
+            return True
+        except: return False
+
+    @colour_channels.getter
+    def red_channel(self):
+        try:
+            cv2.imshow("red", self._colour_channels["RED"])
+            cv2.waitKey(0)
+            cv2.destroyWindow("red")
+            return True
+        except: return False
+
+
+    @colour_channels.getter
+    def green_channel(self):
+        try:
+            cv2.imshow("green", self._colour_channels["GREEN"])
+            cv2.waitKey(0)
+            cv2.destroyWindow("green")
+            return True
+        except: return False
+
+    @colour_channels.getter
+    def blue_channel(self):
+        try:
+            cv2.imshow("blue", self._colour_channels["BLUE"])
+            cv2.waitKey(0)
+            cv2.destroyWindow("blue")
+            return True
+        except: return False
 
     # ProcessImage shouldn't be async as otherwise it might return variables with no value
     # should return maps of the image outlining each specific colour
     def _process_image(self) -> None:
         print("image received is", self._received_image)
-        cv2.imshow("image", self._received_image)
-        cv2.waitKey(delay=3)
+
+        red_only = self._received_image.copy()
+        # blue channel = [:, :, 0] green channel = [:, :, 1] red channel = [:, :, 2]
+        # set blue and green channels to 0
+        red_only[:, :, 0] = 0
+        red_only[:, :, 1] = 0
+
+        green_only = self._received_image.copy()
+        # set blue and red channels to 0
+        green_only[:, :, 0] = 0
+        green_only[:, :, 2] = 0
+
+        blue_only = self._received_image.copy()
+        # set green and red channels to 0
+        blue_only[:, :, 1] = 0
+        blue_only[:, :, 2] = 0
+
+        self._colour_channels["BASE"] = self._received_image
+        self._colour_channels["RED"] = red_only
+        self._colour_channels["GREEN"] = green_only
+        self._colour_channels["BLUE"] = blue_only
+
         # get colour maps
         self._received_image = None
         print("processed image")
--- main.py
+++ main.py
@@ -13,11 +13,19 @@ def main():
 
     # use while loop to keep it active and looping
     while keep_looping:
-        inp = input("enter command")
-        if inp == "enter":
+        inp = input("enter command > ")
+        if inp == "ei": # enter image
             pass_image_to_color_detec("test.jpg")
-        elif inp == "esc":
+        elif inp == "esc": # escape
             keep_looping = False
+        elif inp == "red": # red channel
+            success = col_detector.red_channel
+        elif inp == "blue": # blue channel
+            success = col_detector.blue_channel
+        elif inp == "green": # green channel
+            success = col_detector.green_channel
+        elif inp == "base": # base channel
+            success = col_detector.base_channel
 
 
 if __name__ == '__main__':
