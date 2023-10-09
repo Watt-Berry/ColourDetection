e0b7fcd427759710d80e62766beb35922e15a896
Sepehr <51531251+Sepehr-Boz@users.noreply.github.com>
2023-10-09 18:02:28 +0100

    deleted files other than main and colour_detector, as am only coding the color detector right nothing else. main is used for testing colour detector by passing image paths to it.

--- colour_detector.py
+++ colour_detector.py
@@ -1,14 +1,13 @@
 # create class to detect a certain colour from image and return coordinates
-import asyncio
-import threading
 import time
+
+import cv2
 
 
 class ColourDetector:
     def __init__(self):
         # public so any outside code should be able to add an image to it
         self._received_image = None
-        self._loop_i = 1
 
     # make the received_image variable a property
     @property
@@ -18,16 +17,16 @@ class ColourDetector:
     # add a 'listener' to the variable so that whenever a new image is passed it gets immediately processed and any
     # values are returned
     @image_to_process.setter
-    def image_to_process(self, value: int):
-        self._received_image = value
-        print(self._loop_i)
-        self._loop_i += 1
-
+    def image_to_process(self, value: str):
+        self._received_image = cv2.imread(value, flags=cv2.IMREAD_GRAYSCALE)
         self._process_image()
 
     # ProcessImage shouldn't be async as otherwise it might return variables with no value
     # should return maps of the image outlining each specific colour
     def _process_image(self) -> None:
+        print("image received is", self._received_image)
+        cv2.imshow("image", self._received_image)
+        cv2.waitKey(delay=3)
         # get colour maps
         self._received_image = None
         print("processed image")
--- main.py
+++ main.py
@@ -1,36 +1,23 @@
-import asyncio
-import threading
 import time
 
-from camera import Camera
 from colour_detector import ColourDetector
-from input_handler import InputHandler
-from pynput.keyboard import Key
 
 
 def main():
-    camera = Camera()
-    inp_handler = InputHandler()
     col_detector = ColourDetector()
-    image = 0
-
     keep_looping = True
 
-    def esc_main():
-        nonlocal keep_looping
-        keep_looping = False
-
-    def pass_image_to_color_detec():
+    def pass_image_to_color_detec(image):
         nonlocal col_detector
         col_detector.image_to_process = image
 
-    inp_handler.attach_func_to_keypress(Key.enter, pass_image_to_color_detec)
-    inp_handler.attach_func_to_keypress(Key.esc, esc_main)
-
     # use while loop to keep it active and looping
     while keep_looping:
-        image += 1
-        time.sleep(1)
+        inp = input("enter command")
+        if inp == "enter":
+            pass_image_to_color_detec("test.jpg")
+        elif inp == "esc":
+            keep_looping = False
 
 
 if __name__ == '__main__':
